f958289cb70544e51f22cfa7636bbb0cabe3f0a3
Caio <caio-pimentel@hotmail.com>
2024-10-04 10:42:55 -0300

    Mentoria e Curso criados.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import com.caiopimentel.dominio.Curso;
+import com.caiopimentel.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("JAVA");
+        curso1.setDescricao("Curso de JAVA!");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("PYTHON");
+        curso2.setDescricao("Curso de PYTHON!");
+        curso2.setCargaHoraria(5);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria JAVA.");
+        mentoria.setDescricao("Descrição mentoria JAVA.");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
